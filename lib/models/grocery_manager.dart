0754cda2e87951b3dd3f70a436c97a5a9942532c
Gelin Eguinosa <eguinosa@gmail.com>
2023-09-06 17:24:27 -0400

    Add the GroceryManager() model.

---
+++ lib/models/grocery_manager.dart
@@ -0,0 +1,45 @@
+// Gelin Eguinosa
+// 2023
+
+import 'package:flutter/material.dart';
+
+import 'grocery_item.dart';
+
+
+class GroceryManager extends ChangeNotifier {
+  final _groceryItems = <GroceryItem>[];
+  List<GroceryItem> get groceryItems => List.unmodifiable(_groceryItems);
+
+  String getItemId(int index) {
+    final groceryItem = _groceryItems[index];
+    return groceryItem.id;
+  }
+
+  GroceryItem? getGroceryItem(String id) {
+    final index = _groceryItems.indexWhere((element) => element.id == id);
+    if (index == -1) return null;
+    return groceryItems[index];
+  }
+
+  void deleteItem(int index) {
+    _groceryItems.removeAt(index);
+    notifyListeners();
+  }
+
+  void addItem(GroceryItem item) {
+    _groceryItems.add(item);
+    notifyListeners();
+  }
+
+  void updateItem(GroceryItem item) {
+    final index = _groceryItems.indexWhere((element) => element.id == item.id);
+    _groceryItems[index] = item;
+    notifyListeners();
+  }
+
+  void completeItem(int index, bool change) {
+    final item = _groceryItems[index];
+    _groceryItems[index] = item.copyWith(isComplete: change);
+    notifyListeners();
+  }
+}
