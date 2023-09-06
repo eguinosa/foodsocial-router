baf146b0814fd981abfe28c7a0f230dc1b46f8ae
Gelin Eguinosa <eguinosa@gmail.com>
2023-09-06 14:47:41 -0400

    Add the ExplorScreen() widget

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,42 @@
+// Gelin Eguinosa
+// 2023
+
+import 'package:flutter/material.dart';
+
+import '../api/mock_foodsocial_service.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFoodSocialService();
+
+  ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot){
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(
+                  recipes: snapshot.data?.todayRecipes ?? [],
+              ),
+              const SizedBox(height: 16),
+              FriendPostListView(
+                friendPosts: snapshot.data?.friendPosts ?? [],
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
