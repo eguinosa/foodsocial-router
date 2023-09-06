299918800b32fd3fd106d6cbf2c8d459b04510d6
Gelin Eguinosa <eguinosa@gmail.com>
2023-09-06 17:09:37 -0400

    Add the ExploreData() model.

---
+++ lib/models/explore_data.dart
@@ -0,0 +1,16 @@
+// Gelin Eguinosa
+// 2023
+
+import 'explore_recipe.dart';
+import 'post.dart';
+
+
+class ExploreData {
+  final List<ExploreRecipe> todayRecipes;
+  final List<Post> friendPosts;
+
+  ExploreData(
+    this.todayRecipes,
+    this.friendPosts,
+  );
+}
